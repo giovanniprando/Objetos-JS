0669d0fc74fefd9c273841976fce791113d080ff
Giovanni Prando <giovanni.prando615@gmail.com>
2026-09-11 16:47:32 -0300

    first commit

---
+++ exercicios.js
@@ -0,0 +1,92 @@
+//exercicio 1
+/* 
+let pessoa = {
+  nome: "Giovanni",
+  idade: 17,
+  cidade: "São Paulo",
+};
+
+console.log("Nome: " + pessoa.nome);
+console.log("Idade: " + pessoa.idade);
+console.log("Cidade: " + pessoa.cidade);
+ */
+
+//exercicio 2
+/* 
+let produto = {
+  nome: "Iphone Duo",
+  preco: 22000,
+};
+
+console.log("Produto: " + produto.nome);
+console.log("Preço: " + produto.preco);
+
+produto.preco = 30000;
+
+ */
+//exercicio 3
+/* 
+let carro = {
+  marca: "Porsche",
+  modelo: "911 Turbo",
+  ano: "2024",
+};
+console.log("Marca do carro: " + carro.marca);
+console.log("Modelo do carro: " + carro.modelo);
+console.log("Ano do carro: " + carro.ano);
+
+carro.cor = "Azul";
+
+console.log("Cor: " + carro.cor);
+ */
+
+//exerciocio 4
+/* 
+let aluno = {
+  nome: "Danilo",
+  nota1: 9,
+  nota2: 7,
+};
+
+let media = (aluno.nota1 + aluno.nota2) / 2;
+
+console.log("Aluno: " + aluno.nome);
+console.log("Media: " + media);
+ */
+
+//exerciocio 5
+/* 
+let computador = {
+  marca: "Dell",
+  processador: "Intel Core Ultra 9",
+  memoria: "16gb",
+};
+console.log("Processador: " + computador.processador);
+
+computador.memoria = "32gb";
+computador.armazenamento = "2 T";
+
+console.log(computador);
+ */
+
+// Nível Médio
+
+//Exercicio 1
+/* 
+
+let aluno = {
+  nome: "João",
+  idade: 19,
+  curso: "Desenvolvimento de Sistemas",
+  nota: 7,
+};
+console.log(aluno.nome);
+console.log(aluno.idade);
+
+aluno.nota = 8;
+aluno.cidade = "Rio de Janeiro";
+
+delete aluno.idade;
+console.log(aluno);
+ */
+// Exercicio 2
